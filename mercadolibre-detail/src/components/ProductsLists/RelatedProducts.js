6cd9c91688ecff95b8d6ad36d7aefc5c01d9f1c3
facc77 <ferrerfacundo96@gmail.com>
2022-05-21 16:41:41 -0300

    productsList section added

---
+++ mercadolibre-detail/src/components/ProductsLists/RelatedProducts.js
@@ -0,0 +1,79 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../../router/AppRouter";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { relatedProducts } from "../../assets/products";
+import ProductCard from "../ProductCard";
+
+const RelatedProducts = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <>
+      <Box
+        sx={{
+          marginX: "3rem",
+          marginY: "2rem",
+          position: "relative",
+          bottom: { sx: 0, md: "10rem" },
+        }}
+      >
+        <Box sx={{ mb: "1rem" }}>
+          <Typography
+            sx={{
+              color: theme ? "#fff" : "#333333",
+              fontSize: "24px",
+              mr: "1rem",
+              display: "inline",
+            }}
+          >
+            Productos relacionados
+          </Typography>
+          <Typography
+            sx={{ color: "#3483fa", fontSize: "16px", display: "inline" }}
+          >
+            Anuncia aquí
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", flexWrap: { xs: "wrap", md: "nowrap" } }}>
+          {relatedProducts[0].map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          marginX: "3rem",
+          marginY: "2rem",
+          position: "relative",
+          bottom: { sx: 0, md: "10rem" },
+        }}
+      >
+        <Box sx={{ mb: "1rem" }}>
+          <Typography
+            sx={{
+              color: theme ? "#fff" : "#333333",
+              fontSize: "24px",
+              mr: "1rem",
+              display: "inline",
+            }}
+          >
+            Quienes vieron este producto también compraron
+          </Typography>
+          <Typography
+            sx={{ color: "#3483fa", fontSize: "16px", display: "inline" }}
+          >
+            Anuncia aquí
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", flexWrap: { xs: "wrap", md: "nowrap" } }}>
+          {relatedProducts[1].map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default RelatedProducts;
---
+++ mercadolibre-detail/src/components/ProductsLists/VendorProducts.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../../router/AppRouter";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import { vendorProducts } from "../../assets/products";
+import ProductCard from "../ProductCard";
+
+const VendorProducts = () => {
+  const theme = useContext(ThemeContext);
+  return (
+    <Grid item xs={12} md={8}>
+      <Box
+        sx={{
+          marginX: "3rem",
+          position: "relative",
+          top: { xs: 0, md: "-10rem" },
+          borderBottom: " 1px solid",
+          borderImage: `linear-gradient(to right,${
+            theme ? "#fff" : "rgba(0,0,0,.1)"
+          }  90%, transparent 20%) 100% 1`,
+        }}
+      >
+        <Typography
+          sx={{
+            color: theme ? "#fff" : "#333333",
+            fontSize: "24px",
+            mb: "1.5rem",
+          }}
+        >
+          Publicaciones del vendedor
+        </Typography>
+        <Box sx={{ display: "flex", flexWrap: { xs: "wrap", md: "nowrap" } }}>
+          {vendorProducts.map((product) => (
+            <ProductCard key={product.description} product={product} />
+          ))}
+        </Box>
+        <Typography
+          sx={{ color: "#1259c3", fontSize: "14px", marginY: "2.25rem" }}
+        >
+          Ver más publicaciones del vendedor
+        </Typography>
+      </Box>
+    </Grid>
+  );
+};
+
+export default VendorProducts;
